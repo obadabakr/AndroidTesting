94c966848063d2bd44535b9bd54e64e4e85c2a34
obadabakr <obada.bakr@hotmail.com>
2017-12-02 00:47:19 -0800

    first commit

---
+++ WebView/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace WebView1
+{
+    [Activity(Label = "WebView1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            WebView webv1 = FindViewById<WebView>(Resource.Id.webView1);
+            webv1.Settings.JavaScriptEnabled = true;
+            //webv1.LoadUrl("www.google.com");
+            //webv1.LoadUrl("http://news.alruabye.net/");
+            webv1.LoadUrl("file:///android_asset/offers.html");
+
+            webv1.Settings.LoadWithOverviewMode = true;
+            webv1.SetWebViewClient(new HelloWebViewClient());
+
+        }
+    }
+
+    //load url inside
+    public class HelloWebViewClient : WebViewClient
+    {
+        public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        {
+            view.LoadUrl(url);
+            return true;
+        }
+        public override void OnPageFinished(WebView view, string url)
+        {
+
+        }
+    }
+}
+
---
+++ WebView/obj/Debug/android/src/md5db703ec54e4c66c4f79d8dcd2dfc8a84/HelloWebViewClient.java
@@ -0,0 +1,56 @@
+package md5db703ec54e4c66c4f79d8dcd2dfc8a84;
+
+
+public class HelloWebViewClient
+	extends android.webkit.WebViewClient
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_shouldOverrideUrlLoading:(Landroid/webkit/WebView;Ljava/lang/String;)Z:GetShouldOverrideUrlLoading_Landroid_webkit_WebView_Ljava_lang_String_Handler\n" +
+			"n_onPageFinished:(Landroid/webkit/WebView;Ljava/lang/String;)V:GetOnPageFinished_Landroid_webkit_WebView_Ljava_lang_String_Handler\n" +
+			"";
+		mono.android.Runtime.register ("WebView1.HelloWebViewClient, WebView1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", HelloWebViewClient.class, __md_methods);
+	}
+
+
+	public HelloWebViewClient () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == HelloWebViewClient.class)
+			mono.android.TypeManager.Activate ("WebView1.HelloWebViewClient, WebView1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public boolean shouldOverrideUrlLoading (android.webkit.WebView p0, java.lang.String p1)
+	{
+		return n_shouldOverrideUrlLoading (p0, p1);
+	}
+
+	private native boolean n_shouldOverrideUrlLoading (android.webkit.WebView p0, java.lang.String p1);
+
+
+	public void onPageFinished (android.webkit.WebView p0, java.lang.String p1)
+	{
+		n_onPageFinished (p0, p1);
+	}
+
+	private native void n_onPageFinished (android.webkit.WebView p0, java.lang.String p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
